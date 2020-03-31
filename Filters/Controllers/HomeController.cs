b2bbab039558f5a6e762c44fdc9e2af52d32671f
CaoShangXi <1427026071qq@sina.com>
2020-03-31 23:42:31 +0800

    项目初始化

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [CustomAuth(true)]
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+    }
+}
---
+++ Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    /// <summary>
+    /// 自定义授权过滤器
+    /// </summary>
+    public class CustomAuthAttribute:AuthorizeAttribute
+    {
+        private bool localAllowed;
+        public CustomAuthAttribute(bool allowedParam)
+        {
+            localAllowed = allowedParam;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            /*如果是本地请求，即浏览器与应用程序服务器在同一设备上运行而形成的请求*/
+            if (httpContext.Request.IsLocal)
+            {
+                return localAllowed;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
