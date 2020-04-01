9b08faebe8c76fff4188ccae713a5fe6d5808b69
CaoShangXi <1427026071qq@sina.com>
2020-04-02 00:37:40 +0800

    自定义认证过滤器

---
+++ Filters/Infrastructure/GoogleAuthAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+using System.Security.Principal;
+
+namespace Filters.Infrastructure
+{
+    /// <summary>
+    /// 自定义认证过滤器
+    /// </summary>
+    public class GoogleAuthAttribute : FilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext context)
+        {
+            IIdentity ident=context.Principal.Identity;//当前主体的标识
+            if (!ident.IsAuthenticated||!ident.Name.EndsWith("@google.com"))
+            {
+                context.Result=new HttpUnauthorizedResult();//为Result属性设置了一个值，那么MVC框架将调用OnAuthenticationChallenge方法
+            }
+        }
+
+        /// <summary>
+        /// 无论对认证的请求或对动作方法授权策略的请求失败，MVC框架都调用OnAuthenticationChallenge方法
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext context)
+        {
+            if (context.Result == null ||context.Result is HttpUnauthorizedResult)
+            {
+                context.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary
+                    {
+                        { "controller","GoogleAccount"},
+                        { "action","Login"},
+                        { "returnUrl",context.HttpContext.Request.RawUrl}
+                    }
+                    );
+            }
+        }
+    }
+}
