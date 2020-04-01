155366c4f5ba129025b2be55884eee35c7ddc06e
CaoShangXi <1427026071qq@sina.com>
2020-04-02 00:36:48 +0800

    使用控制器演示认证过滤器的使用

---
+++ Filters/Controllers/GoogleAccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GoogleAccountController : Controller
+    {
+        // GET: GoogleAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            if (username.EndsWith("@google.com")&&password=="secret")
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return Redirect(returnUrl ?? Url.Action("Admin", "Index"));
+            }
+            else
+            {
+                ModelState.AddModelError("", "Incorrect username or password");
+                return View();
+            }
+        }
+    }
+}
