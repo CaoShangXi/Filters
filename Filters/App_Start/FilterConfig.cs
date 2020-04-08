161275f8b417604bd2fc0a21ccae63b1875d7fd7
CaoShangXi <1427026071qq@sina.com>
2020-04-09 00:43:21 +0800

    定义全局过滤器

---
+++ Filters/App_Start/FilterConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters
+{
+    /// <summary>
+    /// 定义全局过滤器，要使过滤器生效必须在Global.asax文件设置
+    /// </summary>
+    public class FilterConfig
+    {
+        public static void GegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());//添加异常过滤器
+            filters.Add(new ProfileAllAttribute());//添加动作和结果过滤器
+        }
+    }
+}
--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Filters.Infrastructure;
+using System.Diagnostics;
 
 namespace Filters.Controllers
 {
     public class HomeController : Controller
     {
+        private Stopwatch timer;
         //[CustomAuth(true)]
         //public string Index()
         //{
@@ -59,10 +61,32 @@ namespace Filters.Controllers
         //    return "This is the FilterTest action";
         //}
 
-        [ProfileAction]//动作过滤器，演示动作方法执行完后执行过滤器方法
+        //[ProfileAction]//动作过滤器，演示动作方法执行完后执行过滤器方法
+        //[ProfileResult]//结果过滤器
+        //[ProfileAll]//动作过滤器和结果过滤器
+        //public string FilterTest()
+        //{
+        //    return "This is the ActionFilterTest action";
+        //}
+
+        /// <summary>
+        /// 使用无注解的过滤器方法，会调用下方的OnActionExcuting和OnResultExecuted方法
+        /// </summary>
+        /// <returns></returns>
         public string FilterTest()
         {
             return "This is the ActionFilterTest action";
+        }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        protected override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            timer.Stop();
+            filterContext.HttpContext.Response.Write(string.Format("<div>Total elapsed time：{0:F6}</div>", timer.Elapsed.TotalSeconds));
         }
     }
 }
